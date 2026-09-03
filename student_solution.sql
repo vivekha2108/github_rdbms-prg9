c5edd408600b1b2ccbc159bd1ddf9f9cf2989d79
VIVEKHA E <vivekha2108@gmail.com>
2026-09-03 10:10:56 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,24 @@
+USE vivekha;
+CREATE TABLE Department (
+DepartmentID INT,
+DepartmentName VARCHAR(30)
+);
+INSERT INTO Department VALUES
+(101,'Computer Science'),
+(102,'Mathematics'),
+(103,'Physics');
+CREATE TABLE Student(
+  StudentID INT,
+  StudentName VARCHAR(20),
+  DepartmentID INT
+);
+INSERT INTO Student VALUES
+(1001,'Arun',101),
+(1002,'Divya',102),
+(1003,'Karthik',103);
+SELECT Student.studentName,
+   Department.DepartmentName
+FROM Student
+INNER JOIN department
+ON Student.DepartmentID=Department.DepartmentID;
 
